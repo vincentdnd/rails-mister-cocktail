d7f72b9fdc1ca691a6dcf738585da3781fb606a5
Vincent Donneaud vincent.donneaud@gmail.com <vincent.donneaud@gmail.com>
2018-11-23 17:18:36 +0100

    done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   mount_uploader :photo, PhotoUploader
 end
